fc610b1efe43aedaa3507b93ced99a7d569f402c
songhaein <thdalwh3867@gmail.com>
2024-02-15 01:42:58 +0900

    add jax cnn model

--- ModeldiffJax/modeldiffjaxCNN.py
+++ ModeldiffJax/modeldiffjaxCNN.py
@@ -1,3 +1,24 @@
 # Jax porting modeldiff-CNN
 
+class_names = training_set.class_names
+num_classes = len(class_names)
+
+class CNN(nn.Module):
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(features=128, kernel_size=(3, 3))(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
+        x = nn.Conv(features=64, kernel_size=(3, 3))(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
+        x = nn.Conv(features=32, kernel_size=(3, 3))(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
+        x = x.reshape((x.shape[0], -1))  # flatten
+        x = nn.Dense(features=256)(x)
+        x = nn.Dense(features=128)(x)
+        x = nn.relu(x)
+        x = nn.Dense(features=num_classes)(x)
+        return x
 # Quantization
